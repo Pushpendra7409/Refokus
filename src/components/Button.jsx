0e13ac65e7c87f0742baff439c1a4d841ab368eb
Pushpendra7409 <pushpendra7409@gmail.com>
2024-09-26 09:57:07 +0530

    Intial file and Folder Structure with Navbar

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsArrowReturnRight } from "react-icons/bs";
+
+function Button() {
+  return (
+    <div className='min-w-40 p-1 bg-zinc-100 text-black rounded-full'>
+        <span style={{ marginLeft: 15 }} className='text-base px-1 py-1 font-semi-bold flex items-center gap-5'>Get started
+        <BsArrowReturnRight/></span>
+        
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './Button'
+import { PiArrowLineUpBold } from "react-icons/pi";
+
+function Navbar() {
+  return (
+    <div className='max-w-screen-xl mx-auto py-6 flex items-center justify-between border-b-[1px] border-zinc-700'>
+      <div className='nleft flex items-center'>
+      <h1 className='text-xl flex items-center gap-2'><PiArrowLineUpBold/>Refokus</h1>
+      <div className='Links flex gap-14 ml-20'>
+        {["Home", "Work", "Culture", "", "News"].map((elem, index)=> 
+        elem.length === 0? <span className='w-[2px] h-7 bg-zinc-700'></span> : (
+        <a className='text-xl flex items-center gap-1' href='#'>
+        {index === 1 ? <span style={{boxShadow: "0 0 0.25em #00FF19"}} className='inline-block w-1.5 h-1.5 rounded-full bg-green-500 '></span> : null}
+          {elem} </a>))}
+      </div>
+      </div>
+      <Button/>
+    </div>
+  )
+}
+
+export default Navbar
